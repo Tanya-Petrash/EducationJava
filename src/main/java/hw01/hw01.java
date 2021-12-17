0164de5d538d6c142117504b3c45eb60c1d33bab
Petrash Tanya <tetianap@wizardsdev.com>
2021-12-17 14:08:21 +0200

    HW1 exercise 1

--- src/main/java/hw01/hw01.java
+++ src/main/java/hw01/hw01.java
@@ -2,15 +2,22 @@ package hw01;
 
 public class hw01 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (i == j) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
-                }
-            }
-            System.out.println(""); // ln  переводит на новую строчку
+        int firstnumber = 85;
+        int secondnumber = 3;
+        int thirdnumber = 5;
+        int fourthnumber = 1000;
+
+        if (firstnumber < secondnumber && firstnumber < thirdnumber && firstnumber < fourthnumber) {
+            System.out.print(firstnumber);
+        } else if (secondnumber < firstnumber && secondnumber < thirdnumber && secondnumber < fourthnumber) {
+            System.out.print(secondnumber);
+        } else if (thirdnumber < firstnumber && thirdnumber < secondnumber && thirdnumber < fourthnumber) {
+            System.out.print(thirdnumber);
+        } else if (fourthnumber < firstnumber && fourthnumber < secondnumber && fourthnumber < thirdnumber) {
+            System.out.print(fourthnumber);
+        } else {
+            System.out.print("Mistake");
         }
     }
 }
+
