190435fa50388b2cc9a3beca84cc73f9158e955c
Petrash Tanya <tetianap@wizardsdev.com>
2021-12-17 14:14:05 +0200

    HW1 exercise 2

--- src/main/java/hw01/hw01.java
+++ src/main/java/hw01/hw01.java
@@ -2,19 +2,19 @@ package hw01;
 
 public class hw01 {
     public static void main(String[] args) {
-        // Даны 4 числа типа int. Сравнить их и вывести наименьшее на консоль.
+        // Вывести на консоль количество максимальных чисел среди этих четырех
         int firstnumber = 85;
         int secondnumber = 3;
         int thirdnumber = 5;
         int fourthnumber = 1000;
 
-        if (firstnumber < secondnumber && firstnumber < thirdnumber && firstnumber < fourthnumber) {
+        if (firstnumber  > secondnumber && firstnumber > thirdnumber && firstnumber >fourthnumber) {
             System.out.print(firstnumber);
-        } else if (secondnumber < firstnumber && secondnumber < thirdnumber && secondnumber < fourthnumber) {
+        } else if (secondnumber > firstnumber && secondnumber > thirdnumber && secondnumber > fourthnumber) {
             System.out.print(secondnumber);
-        } else if (thirdnumber < firstnumber && thirdnumber < secondnumber && thirdnumber < fourthnumber) {
+        } else if (thirdnumber > firstnumber && thirdnumber > secondnumber && thirdnumber > fourthnumber) {
             System.out.print(thirdnumber);
-        } else if (fourthnumber < firstnumber && fourthnumber < secondnumber && fourthnumber < thirdnumber) {
+        } else if (fourthnumber > firstnumber && fourthnumber > secondnumber && fourthnumber > thirdnumber) {
             System.out.print(fourthnumber);
         } else {
             System.out.print("Mistake");
