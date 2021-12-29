041057078a1e46ca97797cdfd53c7bda2b2513ef
Petrash Tanya <tetianap@wizardsdev.com>
2021-12-29 16:55:04 +0200

    HW 2 exe 2 and 3

--- src/main/java/hw01/Hw01.java
+++ src/main/java/hw01/Hw01.java
@@ -8,7 +8,177 @@ public class Hw01 {
     public static void main(String[] args) {
         // Ctrl Alt L выровнять
         // displayTheHighestAndSmallNumber(new ArrayList<>(Arrays.asList(85, 3, 5, 1000, 2596)));
-        rombHighLineFigure();
+        puzzle();
+    }
+
+    public static void puzzle() {
+        Scanner scan = new Scanner(System.in); // для ввода в консоль
+        System.out.print("Может ли дождь лить два дня подряд?\n");
+        System.out.print("Enter answer\n");
+        String answer = scan.nextLine();
+        for (int i = 1; i <= 3; i++) {
+            if (answer.contentEquals("н") || answer.contentEquals("no") || answer.contentEquals(
+                "нет") || answer.contentEquals("n")) {
+                System.out.print("Right\n");
+                break;
+            } else if (answer.contentEquals("Сдаюсь")) {
+                System.out.print("Right answer is no\n");
+                break;
+            } else if (i <= 3) {
+                System.out.print("Try again\n");
+                String text = scan.nextLine();
+                if (text.contentEquals("н") || text.contentEquals("no") ||
+                    text.contentEquals("нет") || text.contentEquals("n")) {
+                    System.out.print("Right\n");
+                    break;} else if(text.contentEquals("Сдаюсь")) {
+                    System.out.print("Right answer is no\n");
+                    break;} else {
+                    System.out.print("Game over\n");
+                }
+            }
+        }
+    }
+
+    public static void fibonacci() {
+        int n1 = 1; // первое число начинается с 1
+        int n2 = 1; // второе число начинается с 1
+        int n3;
+        System.out.print(n1 + " " + n2 + " ");
+        for (int i = 3; i <= 11; i++) { //  i = 3 так как первые два числа уже указала в условии
+            n3 = n1 + n2;
+            System.out.print(n3 + " ");
+            n1 = n2;
+            n2
+                = n3; // значение n3 присваивается к n2 после чего след этап в for n2 уже идет с новым значением
+        }
+        System.out.println();
+    }
+
+    public static void recumbentRhombus() { //реализовать
+        for (int i = 0; i < 10; i++) {
+            for (int k = 0; k < 10; k++) {
+                if (i <= k && i <= 9 - k) {
+                    System.out.print("* ");
+                } else if (!(i + 1 <= k || i + 1 <= 9 - k)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void emptySquare() {  //реализовать
+        for (int i = 0; i < 10; i++) {
+            for (int k = 0; k < 10; k++) {
+                if (i >= 9 - k) {
+                    System.out.print("* ");
+                } else if (i <= 9 - k) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void highToDownRectangularTriangle() {
+        for (int i = 0; i < 10; i++) {
+            for (int k = 0; k < 10; k++) {
+                if (i <= 9 - k) {
+                    System.out.print("* ");
+                } else if (i <= 9 - k) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void highRectangularTriangle() {
+        for (int i = 0; i < 10; i++) {
+            for (int k = 0; k < 10; k++) {
+                if (9 - i <= k) {
+                    System.out.print("* ");
+                } else if (9 - i <= k) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void downRectangularTriangle() {
+        for (int i = 0; i < 10; i++) {
+            for (int k = 0; k < 10; k++) {
+                if (i <= k) {
+                    System.out.print("* ");
+                } else if (i - 9 >= k) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void stickBottomToHigh() {
+        for (int i = 0; i < 10; i++) {
+            for (int k = 0; k < 10; k++) {
+                if (i == k) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void stickHighToBottom() {
+        for (int i = 0; i < 10; i++) {
+            for (int k = 0; k < 10; k++) {
+                if (i == 9 - k) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void rectangle() { // прямоугольник
+        for (int i = 0; i < 10; i++) {
+            for (int k = 0; k < 10; k++) {
+                if (i >= k - 9) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void rectangularTriangle() {
+        for (int i = 0; i < 10; i++) {
+            for (int k = 0; k < 10; k++) {
+                if (i >= k) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
     }
 
     public static void topDownHighLineFigure() {
@@ -50,7 +220,7 @@ public class Hw01 {
         }
     }
 
-    public static void rombHighLineFigure() {
+    public static void rhombus() {
         for (int i = 0; i < 10; i++) {
             for (int k = 0; k < 10; k++) {
                 if (i <= k && i <= 9 - k) {
@@ -78,7 +248,7 @@ public class Hw01 {
         }
     }
 
-    public static void highttodown_line_figures() {
+    public static void hightToDownLine_figures() {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 if (i == j) {
